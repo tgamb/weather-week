a40c3f1c324824414b5633b0511f8eb8699fcf72
Thomas Gamble <thomasgamble8170@gmail.com>
2019-11-26 08:50:33 -0500

    Implemented Open Weather API

---
+++ client/src/components/WeatherDashboard.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class WeatherDashboard extends Component {
+    state = {
+        weather: {}
+    }
+
+    componentDidMount() {
+        this.getWeather()
+    }
+
+    getWeather = () => {
+        axios.get('http://api.openweathermap.org/data/2.5/weather?zip=30327,us&units=imperial&APPID=1aa2fe15d0a2c23dca1307993fb1d38c')
+            .then((res) => {
+                console.log(res.data)
+                this.setState({weather: res.data})
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
