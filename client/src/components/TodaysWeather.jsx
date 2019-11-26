569a32c24a4b514105acdf81dd0d07d481599a97
Thomas Gamble <thomasgamble8170@gmail.com>
2019-11-26 09:26:09 -0500

    Addded Today's Weather component

---
+++ client/src/components/TodaysWeather.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodaysWeather extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
